d9ffec80d5f50ad77f29d154ed19e226fd121945
koxrel <koxrel@outlook.com>
2016-03-01 12:15:07 +0300

    BotWebRequest: GetUpdates

--- LearningAssistant.TelegramBot/BotWebRequest.cs
+++ LearningAssistant.TelegramBot/BotWebRequest.cs
@@ -12,6 +12,19 @@ namespace LearningAssistant.TelegramBot
 {
     public class BotWebRequest
     {
+        private const string Token = "*";
+        private const string Keyboard = @"{""keyboard"":[[""/homework_ielts"",""/homework_infotech""],[""/deadlines""]],""resize_keyboard"":true}";
 
+        private readonly HttpClient _client = new HttpClient();
+
+        private int _lastUpdateId;
+        private CancellationTokenSource _cts;
+
+        private Updates GetUpdates()
+        {
+            var response = _client.GetAsync($"https://api.telegram.org/bot{Token}/getupdates?offset={_lastUpdateId}").Result;
+
+            return JsonConvert.DeserializeObject<Updates>(response.Content.ReadAsStringAsync().Result);
+        }
     }
 }
