76c2727dd6fd87f4fd32334e95121311bdc6b760
koxrel <koxrel@outlook.com>
2016-03-01 12:52:41 +0300

    Data Annotations

--- LearningAssistant.Database/Entities/Deadline.cs
+++ LearningAssistant.Database/Entities/Deadline.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,13 @@ namespace LearningAssistant.Database.Entities
     public class Deadline
     {
         public int Id { get; set; }
+        [Required]
         public DateTime DueDate { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Subject { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string Description { get; set; }
 
         public override string ToString()
--- LearningAssistant.Database/Entities/User.cs
+++ LearningAssistant.Database/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace LearningAssistant.Database.Entities
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string FullName { get; set; }
+        [Required]
         public int ChatId { get; set; }
     }
 }
