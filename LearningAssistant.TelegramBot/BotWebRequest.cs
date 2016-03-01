d09cec6df89d0ce1de4d5551900b3c63ccb87f2a
koxrel <koxrel@outlook.com>
2016-03-01 12:10:43 +0300

    BotWebRequest and DTO

--- LearningAssistant.TelegramBot/BotWebRequest.cs
+++ LearningAssistant.TelegramBot/BotWebRequest.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using BotInCloud.DTO;
+using Newtonsoft.Json;
 
 namespace LearningAssistant.TelegramBot
 {
-    public class Class1
+    public class BotWebRequest
     {
+
     }
 }
---
+++ LearningAssistant.TelegramBot/DTO/Update.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace BotInCloud.DTO
+{
+    public class Update
+    {
+        [JsonProperty("update_id")]
+        public int UpdateID { get; set; }
+        [JsonProperty("message")]
+        public Message Message { get; set; }
+    }
+}
