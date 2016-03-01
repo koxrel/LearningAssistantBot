bbea22d5313f52461b55e51c12311e32507e282b
koxrel <koxrel@outlook.com>
2016-03-01 12:45:15 +0300

    DbSet's to Context

--- LearningAssistant.Database/Context.cs
+++ LearningAssistant.Database/Context.cs
@@ -4,11 +4,16 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearningAssistant.Database.Entities;
 
 namespace LearningAssistant.Database
 {
     public class Context : DbContext
     {
         public Context() : base("BotDatabase") { }
+
+        public DbSet<Deadline> Deadlines { get; set; }
+        public DbSet<Hometask> Hometasks { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
