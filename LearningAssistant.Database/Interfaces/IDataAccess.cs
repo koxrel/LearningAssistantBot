80a7c540ad39eb97ab3037068083636fc7cae44c
koxrel <koxrel@outlook.com>
2016-03-12 00:29:01 +0300

    IDataAccess Interface

---
+++ LearningAssistant.Database/Interfaces/IDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using LearningAssistant.Database.Entities;
+
+namespace LearningAssistant.Database
+{
+    public interface IDataAccess
+    {
+        void AddDeadline(Deadline deadline);
+        void AddHometask(Hometask hometask);
+        void AddUser(User user);
+        void Dispose();
+        IEnumerable<Deadline> GetCurrentDeadlines();
+        Hometask GetCurrentIeltsHometask();
+        Hometask GetCurrentInfoTechHometask();
+        IEnumerable<Deadline> GetDeadlines();
+        IEnumerable<Hometask> GetHomeTasks();
+        void RemoveOldRecords();
+    }
+}
