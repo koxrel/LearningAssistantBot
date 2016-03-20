5b34d3e2c4313bf29809dec09d7b465ad8bd082b
koxrel <koxrel@outlook.com>
2016-03-20 19:48:09 +0300

    Updated IBotWebRequest

--- LearningAssistant.TelegramBot/Interfaces/IBotWebRequest.cs
+++ LearningAssistant.TelegramBot/Interfaces/IBotWebRequest.cs
@@ -6,6 +6,8 @@ namespace LearningAssistant.TelegramBot.Interfaces
     public interface IBotWebRequest
     {
         bool IsActive { get; }
+        string BotName { get; }
+        string BotUsername { get; }
 
         event Action OnError;
         Task GetBotName();
